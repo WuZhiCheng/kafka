5a1db95f74baca6d7637a7a20f306f464557db95
Administrator <1067064019@qq.com>
2018-06-10 22:03:32 +0800

    elk连调

--- src/main/java/com/example/kafka/test/KafkaProducer.java
+++ src/main/java/com/example/kafka/test/KafkaProducer.java
@@ -1,5 +1,7 @@
 package com.example.kafka.test;
 
+import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.JSONPObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -7,6 +9,7 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.util.concurrent.ListenableFuture;
 
+import java.util.Date;
 import java.util.UUID;
 
 /**
@@ -26,8 +29,17 @@ public class KafkaProducer {
     @Scheduled(cron = "00/1 * * * * ?")
     public void send(){
         String message = UUID.randomUUID().toString();
-        ListenableFuture future = kafkaTemplate.send("topic-test222", message);
-        future.addCallback(o -> System.out.println("send-消息发送成功：" + message), throwable -> System.out.println("消息发送失败：" + message));
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("age",18);
+        jsonObject.put("time",System.currentTimeMillis());
+        String resule = jsonObject.toJSONString();
+        ListenableFuture future = kafkaTemplate.send("ecplogs", resule);
+        future.addCallback(o -> System.out.println("send-消息发送成功：" + resule),
+                throwable -> System.out.println("消息发送失败：" + resule));
+
+       /* ListenableFuture future = kafkaTemplate.send("ecplogs", "修改/添加"+message);
+        future.addCallback(o -> System.out.println("send-消息发送成功：" + message),
+                throwable -> System.out.println("消息发送失败：" + message+",,"+new Date()));*/
     }
 
 
